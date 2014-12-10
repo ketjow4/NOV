ceddaeb999c075a2eee52d3c800538f0ca6177f4
Wojtek Dudzik <wojtek.adam.dudzik@gmail.com>
2014-12-10 10:57:38 +0100

    up/down altitude change

--- GCSViews/FlightPlanner.Designer.cs
+++ GCSViews/FlightPlanner.Designer.cs
@@ -1289,7 +1289,7 @@
         private System.Windows.Forms.Label label3;
         private System.Windows.Forms.Label label2;
         private System.Windows.Forms.Label Label1;
-        private System.Windows.Forms.TextBox TXT_homealt;
+        internal System.Windows.Forms.TextBox TXT_homealt;
         private System.Windows.Forms.TextBox TXT_homelng;
         private System.Windows.Forms.TextBox TXT_homelat;
         private System.Windows.Forms.DataGridViewImageColumn dataGridViewImageColumn1;
@@ -1300,7 +1300,7 @@
         private System.Windows.Forms.CheckBox CHK_verifyheight;
         private Controls.MyButton BUT_Add;
         private System.Windows.Forms.TextBox TXT_WPRad;
-        private System.Windows.Forms.TextBox TXT_DefaultAlt;
+        internal System.Windows.Forms.TextBox TXT_DefaultAlt;
         private System.Windows.Forms.Label LBL_WPRad;
         private System.Windows.Forms.Label LBL_defalutalt;
         private System.Windows.Forms.TextBox TXT_loiterrad;
