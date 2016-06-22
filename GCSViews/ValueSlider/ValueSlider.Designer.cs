c098118f63eb88fad62284f2a917c0ad0d5399c3
Michał Płatek <michal.platek.hf@gmail.com>
2016-06-22 13:14:59 +0200

    Added ValueSlider form

---
+++ GCSViews/ValueSlider/ValueSlider.Designer.cs
@@ -0,0 +1,340 @@
+﻿namespace MissionPlanner.GCSViews.ValueSlider
+{
+	partial class ValueSlider
+	{
+		/// <summary>
+		/// Required designer variable.
+		/// </summary>
+		private System.ComponentModel.IContainer components = null;
+
+		/// <summary>
+		/// Clean up any resources being used.
+		/// </summary>
+		/// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+		protected override void Dispose(bool disposing)
+		{
+			if (disposing && (components != null))
+			{
+				components.Dispose();
+			}
+			base.Dispose(disposing);
+		}
+
+		#region Windows Form Designer generated code
+
+		/// <summary>
+		/// Required method for Designer support - do not modify
+		/// the contents of this method with the code editor.
+		/// </summary>
+		private void InitializeComponent()
+		{
+			gTrackBar.ColorPack colorPack4 = new gTrackBar.ColorPack();
+			gTrackBar.ColorPack colorPack5 = new gTrackBar.ColorPack();
+			gTrackBar.ColorPack colorPack6 = new gTrackBar.ColorPack();
+			this.tableLayoutPanel1 = new System.Windows.Forms.TableLayoutPanel();
+			this.InfoLabel = new System.Windows.Forms.Label();
+			this.tableLayoutPanel2 = new System.Windows.Forms.TableLayoutPanel();
+			this.InputValue = new System.Windows.Forms.TextBox();
+			this.ButtonOk = new System.Windows.Forms.Button();
+			this.ButtonCancel = new System.Windows.Forms.Button();
+			this.tableLayoutPanel3 = new System.Windows.Forms.TableLayoutPanel();
+			this.trackBar = new gTrackBar.gTrackBar();
+			this.tableLayoutPanel4 = new System.Windows.Forms.TableLayoutPanel();
+			this.buttonMinus1 = new System.Windows.Forms.Button();
+			this.buttonMinus10 = new System.Windows.Forms.Button();
+			this.buttonPlus10 = new System.Windows.Forms.Button();
+			this.buttonPlus1 = new System.Windows.Forms.Button();
+			this.tableLayoutPanel1.SuspendLayout();
+			this.tableLayoutPanel2.SuspendLayout();
+			this.tableLayoutPanel3.SuspendLayout();
+			this.tableLayoutPanel4.SuspendLayout();
+			this.SuspendLayout();
+			// 
+			// tableLayoutPanel1
+			// 
+			this.tableLayoutPanel1.ColumnCount = 5;
+			this.tableLayoutPanel1.ColumnStyles.Add(new System.Windows.Forms.ColumnStyle(System.Windows.Forms.SizeType.Percent, 15F));
+			this.tableLayoutPanel1.ColumnStyles.Add(new System.Windows.Forms.ColumnStyle(System.Windows.Forms.SizeType.Percent, 15F));
+			this.tableLayoutPanel1.ColumnStyles.Add(new System.Windows.Forms.ColumnStyle(System.Windows.Forms.SizeType.Percent, 40F));
+			this.tableLayoutPanel1.ColumnStyles.Add(new System.Windows.Forms.ColumnStyle(System.Windows.Forms.SizeType.Percent, 15F));
+			this.tableLayoutPanel1.ColumnStyles.Add(new System.Windows.Forms.ColumnStyle(System.Windows.Forms.SizeType.Percent, 15F));
+			this.tableLayoutPanel1.Controls.Add(this.InfoLabel, 0, 0);
+			this.tableLayoutPanel1.Dock = System.Windows.Forms.DockStyle.Top;
+			this.tableLayoutPanel1.Location = new System.Drawing.Point(0, 0);
+			this.tableLayoutPanel1.Name = "tableLayoutPanel1";
+			this.tableLayoutPanel1.RowCount = 1;
+			this.tableLayoutPanel1.RowStyles.Add(new System.Windows.Forms.RowStyle(System.Windows.Forms.SizeType.Percent, 33F));
+			this.tableLayoutPanel1.RowStyles.Add(new System.Windows.Forms.RowStyle(System.Windows.Forms.SizeType.Percent, 34F));
+			this.tableLayoutPanel1.RowStyles.Add(new System.Windows.Forms.RowStyle(System.Windows.Forms.SizeType.Percent, 33F));
+			this.tableLayoutPanel1.Size = new System.Drawing.Size(400, 46);
+			this.tableLayoutPanel1.TabIndex = 0;
+			// 
+			// InfoLabel
+			// 
+			this.InfoLabel.AutoSize = true;
+			this.InfoLabel.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(16)))), ((int)(((byte)(17)))), ((int)(((byte)(18)))));
+			this.tableLayoutPanel1.SetColumnSpan(this.InfoLabel, 5);
+			this.InfoLabel.Dock = System.Windows.Forms.DockStyle.Fill;
+			this.InfoLabel.Font = new System.Drawing.Font("Century Gothic", 20.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(238)));
+			this.InfoLabel.ForeColor = System.Drawing.Color.FromArgb(((int)(((byte)(108)))), ((int)(((byte)(206)))), ((int)(((byte)(252)))));
+			this.InfoLabel.Location = new System.Drawing.Point(2, 2);
+			this.InfoLabel.Margin = new System.Windows.Forms.Padding(2);
+			this.InfoLabel.Name = "InfoLabel";
+			this.InfoLabel.Size = new System.Drawing.Size(396, 42);
+			this.InfoLabel.TabIndex = 18;
+			this.InfoLabel.Text = "Question Here";
+			this.InfoLabel.TextAlign = System.Drawing.ContentAlignment.MiddleLeft;
+			// 
+			// tableLayoutPanel2
+			// 
+			this.tableLayoutPanel2.ColumnCount = 3;
+			this.tableLayoutPanel2.ColumnStyles.Add(new System.Windows.Forms.ColumnStyle(System.Windows.Forms.SizeType.Percent, 33F));
+			this.tableLayoutPanel2.ColumnStyles.Add(new System.Windows.Forms.ColumnStyle(System.Windows.Forms.SizeType.Percent, 33F));
+			this.tableLayoutPanel2.ColumnStyles.Add(new System.Windows.Forms.ColumnStyle(System.Windows.Forms.SizeType.Percent, 34F));
+			this.tableLayoutPanel2.Controls.Add(this.InputValue, 0, 0);
+			this.tableLayoutPanel2.Controls.Add(this.ButtonOk, 0, 0);
+			this.tableLayoutPanel2.Controls.Add(this.ButtonCancel, 0, 0);
+			this.tableLayoutPanel2.Dock = System.Windows.Forms.DockStyle.Bottom;
+			this.tableLayoutPanel2.Location = new System.Drawing.Point(0, 139);
+			this.tableLayoutPanel2.Name = "tableLayoutPanel2";
+			this.tableLayoutPanel2.RowCount = 1;
+			this.tableLayoutPanel2.RowStyles.Add(new System.Windows.Forms.RowStyle(System.Windows.Forms.SizeType.Percent, 100F));
+			this.tableLayoutPanel2.Size = new System.Drawing.Size(400, 46);
+			this.tableLayoutPanel2.TabIndex = 1;
+			// 
+			// InputValue
+			// 
+			this.InputValue.BorderStyle = System.Windows.Forms.BorderStyle.None;
+			this.InputValue.Dock = System.Windows.Forms.DockStyle.Fill;
+			this.InputValue.Font = new System.Drawing.Font("Century Gothic", 24F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(238)));
+			this.InputValue.Location = new System.Drawing.Point(266, 2);
+			this.InputValue.Margin = new System.Windows.Forms.Padding(2);
+			this.InputValue.Name = "InputValue";
+			this.InputValue.Size = new System.Drawing.Size(132, 40);
+			this.InputValue.TabIndex = 24;
+			this.InputValue.TextAlign = System.Windows.Forms.HorizontalAlignment.Right;
+			this.InputValue.TextChanged += new System.EventHandler(this.InputValue_TextChanged);
+			// 
+			// ButtonOk
+			// 
+			this.ButtonOk.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(16)))), ((int)(((byte)(17)))), ((int)(((byte)(18)))));
+			this.ButtonOk.Dock = System.Windows.Forms.DockStyle.Fill;
+			this.ButtonOk.FlatAppearance.BorderSize = 0;
+			this.ButtonOk.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
+			this.ButtonOk.Font = new System.Drawing.Font("Century Gothic", 15.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(238)));
+			this.ButtonOk.ForeColor = System.Drawing.Color.White;
+			this.ButtonOk.Location = new System.Drawing.Point(134, 2);
+			this.ButtonOk.Margin = new System.Windows.Forms.Padding(2);
+			this.ButtonOk.Name = "ButtonOk";
+			this.ButtonOk.Size = new System.Drawing.Size(128, 42);
+			this.ButtonOk.TabIndex = 23;
+			this.ButtonOk.Text = "OK";
+			this.ButtonOk.UseVisualStyleBackColor = false;
+			this.ButtonOk.Click += new System.EventHandler(this.ButtonOk_Click);
+			// 
+			// ButtonCancel
+			// 
+			this.ButtonCancel.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(16)))), ((int)(((byte)(17)))), ((int)(((byte)(18)))));
+			this.ButtonCancel.Dock = System.Windows.Forms.DockStyle.Fill;
+			this.ButtonCancel.FlatAppearance.BorderSize = 0;
+			this.ButtonCancel.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
+			this.ButtonCancel.Font = new System.Drawing.Font("Century Gothic", 15.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(238)));
+			this.ButtonCancel.ForeColor = System.Drawing.Color.White;
+			this.ButtonCancel.Location = new System.Drawing.Point(2, 2);
+			this.ButtonCancel.Margin = new System.Windows.Forms.Padding(2);
+			this.ButtonCancel.Name = "ButtonCancel";
+			this.ButtonCancel.Size = new System.Drawing.Size(128, 42);
+			this.ButtonCancel.TabIndex = 22;
+			this.ButtonCancel.Text = "CANCEL";
+			this.ButtonCancel.UseVisualStyleBackColor = false;
+			this.ButtonCancel.Click += new System.EventHandler(this.ButtonCancel_Click);
+			// 
+			// tableLayoutPanel3
+			// 
+			this.tableLayoutPanel3.ColumnCount = 1;
+			this.tableLayoutPanel3.ColumnStyles.Add(new System.Windows.Forms.ColumnStyle(System.Windows.Forms.SizeType.Percent, 50F));
+			this.tableLayoutPanel3.ColumnStyles.Add(new System.Windows.Forms.ColumnStyle(System.Windows.Forms.SizeType.Percent, 50F));
+			this.tableLayoutPanel3.Controls.Add(this.trackBar, 0, 0);
+			this.tableLayoutPanel3.Dock = System.Windows.Forms.DockStyle.Top;
+			this.tableLayoutPanel3.Location = new System.Drawing.Point(0, 46);
+			this.tableLayoutPanel3.Name = "tableLayoutPanel3";
+			this.tableLayoutPanel3.RowCount = 1;
+			this.tableLayoutPanel3.RowStyles.Add(new System.Windows.Forms.RowStyle(System.Windows.Forms.SizeType.Percent, 50F));
+			this.tableLayoutPanel3.RowStyles.Add(new System.Windows.Forms.RowStyle(System.Windows.Forms.SizeType.Percent, 50F));
+			this.tableLayoutPanel3.Size = new System.Drawing.Size(400, 46);
+			this.tableLayoutPanel3.TabIndex = 2;
+			// 
+			// trackBar
+			// 
+			this.trackBar.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(22)))), ((int)(((byte)(23)))), ((int)(((byte)(24)))));
+			colorPack4.Border = System.Drawing.Color.FromArgb(((int)(((byte)(108)))), ((int)(((byte)(206)))), ((int)(((byte)(252)))));
+			colorPack4.Face = System.Drawing.Color.FromArgb(((int)(((byte)(108)))), ((int)(((byte)(206)))), ((int)(((byte)(252)))));
+			colorPack4.Highlight = System.Drawing.Color.FromArgb(((int)(((byte)(108)))), ((int)(((byte)(206)))), ((int)(((byte)(252)))));
+			this.trackBar.ColorDown = colorPack4;
+			colorPack5.Border = System.Drawing.Color.FromArgb(((int)(((byte)(108)))), ((int)(((byte)(206)))), ((int)(((byte)(252)))));
+			colorPack5.Face = System.Drawing.Color.FromArgb(((int)(((byte)(108)))), ((int)(((byte)(206)))), ((int)(((byte)(252)))));
+			colorPack5.Highlight = System.Drawing.Color.FromArgb(((int)(((byte)(108)))), ((int)(((byte)(206)))), ((int)(((byte)(252)))));
+			this.trackBar.ColorHover = colorPack5;
+			colorPack6.Border = System.Drawing.Color.FromArgb(((int)(((byte)(108)))), ((int)(((byte)(206)))), ((int)(((byte)(252)))));
+			colorPack6.Face = System.Drawing.Color.FromArgb(((int)(((byte)(108)))), ((int)(((byte)(206)))), ((int)(((byte)(252)))));
+			colorPack6.Highlight = System.Drawing.Color.FromArgb(((int)(((byte)(108)))), ((int)(((byte)(206)))), ((int)(((byte)(252)))));
+			this.trackBar.ColorUp = colorPack6;
+			this.trackBar.Dock = System.Windows.Forms.DockStyle.Fill;
+			this.trackBar.FloatValueFontColor = System.Drawing.SystemColors.ActiveBorder;
+			this.trackBar.ImeMode = System.Windows.Forms.ImeMode.NoControl;
+			this.trackBar.Label = "";
+			this.trackBar.LabelAlighnment = System.Drawing.StringAlignment.Center;
+			this.trackBar.Location = new System.Drawing.Point(2, 2);
+			this.trackBar.Margin = new System.Windows.Forms.Padding(2);
+			this.trackBar.MaxValue = 360;
+			this.trackBar.MinValue = 1;
+			this.trackBar.Name = "trackBar";
+			this.trackBar.Size = new System.Drawing.Size(396, 42);
+			this.trackBar.SliderShape = gTrackBar.gTrackBar.eShape.Rectangle;
+			this.trackBar.SliderSize = new System.Drawing.Size(15, 30);
+			this.trackBar.SliderWidthHigh = 1F;
+			this.trackBar.SliderWidthLow = 1F;
+			this.trackBar.TabIndex = 55;
+			this.trackBar.TickColor = System.Drawing.Color.White;
+			this.trackBar.TickInterval = 1;
+			this.trackBar.TickThickness = 1F;
+			this.trackBar.UpDownShow = false;
+			this.trackBar.Value = 1;
+			this.trackBar.ValueAdjusted = 1F;
+			this.trackBar.ValueBoxBackColor = System.Drawing.Color.FromArgb(((int)(((byte)(22)))), ((int)(((byte)(23)))), ((int)(((byte)(24)))));
+			this.trackBar.ValueBoxBorder = System.Drawing.Color.FromArgb(((int)(((byte)(22)))), ((int)(((byte)(23)))), ((int)(((byte)(24)))));
+			this.trackBar.ValueBoxFont = new System.Drawing.Font("Century Gothic", 20.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(238)));
+			this.trackBar.ValueBoxFontColor = System.Drawing.Color.White;
+			this.trackBar.ValueBoxSize = new System.Drawing.Size(40, 40);
+			this.trackBar.ValueDivisor = gTrackBar.gTrackBar.eValueDivisor.e1;
+			this.trackBar.ValueStrFormat = null;
+			this.trackBar.ValueChanged += new gTrackBar.gTrackBar.ValueChangedEventHandler(this.trackBar_ValueChanged);
+			// 
+			// tableLayoutPanel4
+			// 
+			this.tableLayoutPanel4.ColumnCount = 4;
+			this.tableLayoutPanel4.ColumnStyles.Add(new System.Windows.Forms.ColumnStyle(System.Windows.Forms.SizeType.Percent, 25F));
+			this.tableLayoutPanel4.ColumnStyles.Add(new System.Windows.Forms.ColumnStyle(System.Windows.Forms.SizeType.Percent, 25F));
+			this.tableLayoutPanel4.ColumnStyles.Add(new System.Windows.Forms.ColumnStyle(System.Windows.Forms.SizeType.Percent, 25F));
+			this.tableLayoutPanel4.ColumnStyles.Add(new System.Windows.Forms.ColumnStyle(System.Windows.Forms.SizeType.Percent, 25F));
+			this.tableLayoutPanel4.Controls.Add(this.buttonPlus1, 0, 0);
+			this.tableLayoutPanel4.Controls.Add(this.buttonPlus10, 0, 0);
+			this.tableLayoutPanel4.Controls.Add(this.buttonMinus10, 0, 0);
+			this.tableLayoutPanel4.Controls.Add(this.buttonMinus1, 0, 0);
+			this.tableLayoutPanel4.Dock = System.Windows.Forms.DockStyle.Top;
+			this.tableLayoutPanel4.Location = new System.Drawing.Point(0, 92);
+			this.tableLayoutPanel4.Name = "tableLayoutPanel4";
+			this.tableLayoutPanel4.RowCount = 1;
+			this.tableLayoutPanel4.RowStyles.Add(new System.Windows.Forms.RowStyle(System.Windows.Forms.SizeType.Percent, 100F));
+			this.tableLayoutPanel4.Size = new System.Drawing.Size(400, 46);
+			this.tableLayoutPanel4.TabIndex = 3;
+			// 
+			// buttonMinus1
+			// 
+			this.buttonMinus1.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(16)))), ((int)(((byte)(17)))), ((int)(((byte)(18)))));
+			this.buttonMinus1.Dock = System.Windows.Forms.DockStyle.Fill;
+			this.buttonMinus1.FlatAppearance.BorderSize = 0;
+			this.buttonMinus1.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
+			this.buttonMinus1.Font = new System.Drawing.Font("Century Gothic", 15.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(238)));
+			this.buttonMinus1.ForeColor = System.Drawing.Color.White;
+			this.buttonMinus1.Location = new System.Drawing.Point(102, 2);
+			this.buttonMinus1.Margin = new System.Windows.Forms.Padding(2);
+			this.buttonMinus1.Name = "buttonMinus1";
+			this.buttonMinus1.Size = new System.Drawing.Size(96, 42);
+			this.buttonMinus1.TabIndex = 77;
+			this.buttonMinus1.Text = "-1";
+			this.buttonMinus1.UseVisualStyleBackColor = false;
+			this.buttonMinus1.Click += new System.EventHandler(this.buttonMinus1_Click);
+			// 
+			// buttonMinus10
+			// 
+			this.buttonMinus10.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(16)))), ((int)(((byte)(17)))), ((int)(((byte)(18)))));
+			this.buttonMinus10.Dock = System.Windows.Forms.DockStyle.Fill;
+			this.buttonMinus10.FlatAppearance.BorderSize = 0;
+			this.buttonMinus10.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
+			this.buttonMinus10.Font = new System.Drawing.Font("Century Gothic", 15.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(238)));
+			this.buttonMinus10.ForeColor = System.Drawing.Color.White;
+			this.buttonMinus10.Location = new System.Drawing.Point(2, 2);
+			this.buttonMinus10.Margin = new System.Windows.Forms.Padding(2);
+			this.buttonMinus10.Name = "buttonMinus10";
+			this.buttonMinus10.Size = new System.Drawing.Size(96, 42);
+			this.buttonMinus10.TabIndex = 83;
+			this.buttonMinus10.Text = "-10";
+			this.buttonMinus10.UseVisualStyleBackColor = false;
+			this.buttonMinus10.Click += new System.EventHandler(this.buttonMinus10_Click);
+			// 
+			// buttonPlus10
+			// 
+			this.buttonPlus10.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(16)))), ((int)(((byte)(17)))), ((int)(((byte)(18)))));
+			this.buttonPlus10.Dock = System.Windows.Forms.DockStyle.Fill;
+			this.buttonPlus10.FlatAppearance.BorderSize = 0;
+			this.buttonPlus10.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
+			this.buttonPlus10.Font = new System.Drawing.Font("Century Gothic", 15.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(238)));
+			this.buttonPlus10.ForeColor = System.Drawing.Color.White;
+			this.buttonPlus10.Location = new System.Drawing.Point(302, 2);
+			this.buttonPlus10.Margin = new System.Windows.Forms.Padding(2);
+			this.buttonPlus10.Name = "buttonPlus10";
+			this.buttonPlus10.Size = new System.Drawing.Size(96, 42);
+			this.buttonPlus10.TabIndex = 84;
+			this.buttonPlus10.Text = "+10";
+			this.buttonPlus10.UseVisualStyleBackColor = false;
+			this.buttonPlus10.Click += new System.EventHandler(this.buttonPlus10_Click);
+			// 
+			// buttonPlus1
+			// 
+			this.buttonPlus1.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(16)))), ((int)(((byte)(17)))), ((int)(((byte)(18)))));
+			this.buttonPlus1.Dock = System.Windows.Forms.DockStyle.Fill;
+			this.buttonPlus1.FlatAppearance.BorderSize = 0;
+			this.buttonPlus1.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
+			this.buttonPlus1.Font = new System.Drawing.Font("Century Gothic", 15.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(238)));
+			this.buttonPlus1.ForeColor = System.Drawing.Color.White;
+			this.buttonPlus1.Location = new System.Drawing.Point(202, 2);
+			this.buttonPlus1.Margin = new System.Windows.Forms.Padding(2);
+			this.buttonPlus1.Name = "buttonPlus1";
+			this.buttonPlus1.Size = new System.Drawing.Size(96, 42);
+			this.buttonPlus1.TabIndex = 85;
+			this.buttonPlus1.Text = "+1";
+			this.buttonPlus1.UseVisualStyleBackColor = false;
+			this.buttonPlus1.Click += new System.EventHandler(this.buttonPlus1_Click);
+			// 
+			// ValueSlider
+			// 
+			this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+			this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+			this.BackColor = System.Drawing.Color.Lime;
+			this.ClientSize = new System.Drawing.Size(400, 185);
+			this.Controls.Add(this.tableLayoutPanel4);
+			this.Controls.Add(this.tableLayoutPanel3);
+			this.Controls.Add(this.tableLayoutPanel2);
+			this.Controls.Add(this.tableLayoutPanel1);
+			this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.None;
+			this.Name = "ValueSlider";
+			this.Text = "ValueSlider";
+			this.TransparencyKey = System.Drawing.Color.Lime;
+			this.tableLayoutPanel1.ResumeLayout(false);
+			this.tableLayoutPanel1.PerformLayout();
+			this.tableLayoutPanel2.ResumeLayout(false);
+			this.tableLayoutPanel2.PerformLayout();
+			this.tableLayoutPanel3.ResumeLayout(false);
+			this.tableLayoutPanel4.ResumeLayout(false);
+			this.ResumeLayout(false);
+
+		}
+
+		#endregion
+
+		private System.Windows.Forms.TableLayoutPanel tableLayoutPanel1;
+		private System.Windows.Forms.TableLayoutPanel tableLayoutPanel2;
+		private System.Windows.Forms.TextBox InputValue;
+		private System.Windows.Forms.Button ButtonOk;
+		private System.Windows.Forms.Button ButtonCancel;
+		private System.Windows.Forms.Label InfoLabel;
+		private System.Windows.Forms.TableLayoutPanel tableLayoutPanel3;
+		private System.Windows.Forms.TableLayoutPanel tableLayoutPanel4;
+		private System.Windows.Forms.Button buttonPlus1;
+		private System.Windows.Forms.Button buttonPlus10;
+		private System.Windows.Forms.Button buttonMinus10;
+		private System.Windows.Forms.Button buttonMinus1;
+		private gTrackBar.gTrackBar trackBar;
+	}
+}
---
+++ GCSViews/ValueSlider/ValueSlider.cs
@@ -0,0 +1,151 @@
+﻿using System;
+using System.Drawing;
+using System.Windows.Forms;
+using MissionPlanner.Validators;
+
+namespace MissionPlanner.GCSViews.ValueSlider
+{
+	public partial class ValueSlider : Form
+	{
+		IValidator<int> Validator;
+		
+		public int Result { get; set; }
+		private bool isValid;
+		
+		public ValueSlider(IValidator<int> validator, string infoLabelText, string initialValue)
+		{
+			Validator = validator;
+			InitializeComponent();
+			InfoLabel.Text = infoLabelText;
+			InputValue.Text = initialValue;
+			trackBar.MinValue = IntValidator.Min;
+			trackBar.MaxValue = IntValidator.Max;
+			SetFonts();
+		}
+		
+		public void SetFonts()
+		{
+			InputValue.Font = new Font("Century Gothic", Modification.ResolutionManager.InputTextBoxFontSize, FontStyle.Regular);
+			InfoLabel.Font = InfoLabel.Font = new Font("Century Gothic", Modification.ResolutionManager.InputInfoFontSize, FontStyle.Regular);
+
+			TableLayoutPanel[] panels = new TableLayoutPanel[] {
+				tableLayoutPanel3,
+				tableLayoutPanel4
+			};
+
+			foreach(TableLayoutPanel panel in panels)
+			{
+				foreach (var element in panel.Controls)
+				{
+					if (element is Button)
+					{
+						(element as Button).Font = new Font("Century Gothic", Modification.ResolutionManager.InputButtonsFontSize, FontStyle.Regular);
+					}
+				}
+			}
+		}
+
+		private void buttonMinus10_Click(object sender, EventArgs e)
+		{
+			setValue(Result - 10);
+		}
+
+		private void buttonMinus1_Click(object sender, EventArgs e)
+		{
+			setValue(Result - 1);
+		}
+
+		private void buttonPlus1_Click(object sender, EventArgs e)
+		{
+			setValue(Result + 1);
+		}
+
+		private void buttonPlus10_Click(object sender, EventArgs e)
+		{
+			setValue(Result + 10);
+		}
+
+		private void trackBar_ValueChanged(object sender, EventArgs e)
+		{
+			setValue(trackBar.Value);
+		}
+
+		private void setValue(int value)
+		{
+			if (!value.ToString().Equals(InputValue.Text))
+			{
+				InputValue.Text = value.ToString();
+			}
+		}
+
+		private void ButtonCancel_Click(object sender, EventArgs e)
+		{
+			DialogResult = DialogResult.Cancel;
+			Close();
+		}
+
+		private void ButtonOk_Click(object sender, EventArgs e)
+		{
+			if (isValid)
+			{
+				DialogResult = DialogResult.OK;
+				Close();
+			}
+			else
+			{
+				CustomMessageBox.Show(String.Format("Value should be between {0} and {1}", Min, Max), "Error", MessageBoxButtons.OK, MessageBoxIcon.Exclamation);
+			}	
+		}
+
+		private void InputValue_TextChanged(object sender, EventArgs e)
+		{
+			isValid = Validator.Validate(InputValue.Text);
+			if (!isValid)
+			{
+				InputValue.BackColor = Color.Red;
+			}
+			else
+			{
+				InputValue.BackColor = Color.FromArgb(255, 255, 255, 255);
+				Result = IntValidator.Value;
+				trackBar.Value = IntValidator.Value;
+			}
+		}
+
+		public NumericValidator<int> IntValidator
+		{
+			get
+			{
+				return Validator as NumericValidator<int>;
+			}
+		}
+		
+		public Size WindowSize
+		{
+			get
+			{
+				return Size;
+			}
+			set
+			{
+				Size = WindowSize;
+			}
+		}
+
+		public int Min
+		{
+			get
+			{
+				return IntValidator.Min;
+			}
+		}
+
+		public int Max
+		{
+			get
+			{
+				return IntValidator.Max;
+			}
+		}
+	}
+}
